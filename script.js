adac99fd7c270f19033f1a96533fe987dd8e660e
baneboz <bmutko@gmail.com>
2023-01-12 18:19:23 +0100

    JS BiT Course Exercises

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 "use strict";
 const btnToggleBg = document.querySelector(".btn__toggle-bg");
 const btnTurnOffToggleBg = document.querySelector(".btn__turn-off");
+
 const msg = document.querySelector(".chat__msg");
 const btnSendMsg = document.querySelector(".btn__send");
+
 const footContainer = document.querySelector(".football__field");
 const footPlayer = document.querySelector(".football__player");
 const btnTurnOffMoving = document.querySelector(".btn__stop-moving");
+
+const marioContainer = document.querySelector(".mario__container");
 const mario = document.querySelector(".mario__img");
 const marioRun = document.querySelector(".mario__img--running");
+
+const galleryContainer = document.querySelector(".gallery__container");
+
 // 1
 const toggleBg = () =>
   document.querySelector("body").classList.toggle("dark-bg");
@@ -50,6 +57,7 @@ btnTurnOffMoving.addEventListener("click", function () {
 // 4
 window.addEventListener("keydown", function (e) {
   if (e.code === "ArrowRight") {
+    marioContainer.style.backgroundPositionX = "10%";
     marioRun.classList.remove("hidden");
     mario.classList.add("hidden");
   }
@@ -60,4 +68,21 @@ window.addEventListener("keyup", function (e) {
     marioRun.classList.add("hidden");
     mario.classList.remove("hidden");
   }
+});
+
+// 4
+galleryContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("gallery__item--large")) {
+    // stop propagation
+    e.stopPropagation();
+  }
+  if (e.target.classList.contains("gallery__item")) {
+    [...e.target.closest(".gallery__container").children].forEach(el =>
+      el.classList.remove("border")
+    );
+    e.target.classList.add("border");
+  }
+});
+document.addEventListener("click", function (e) {
+  console.log(e.target);
 });
